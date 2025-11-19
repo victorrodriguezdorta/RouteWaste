e9049bf469955a409ceb33ca17307a42b26c0fd6
alu0101540153 <alu0101540153@ull.edu.es>
2025-11-19 19:04:28 +0000

    a small change just for check de actions (Issue #19)

--- back-end/src/main/java/es/ull/project/BackEndApplication.java
+++ back-end/src/main/java/es/ull/project/BackEndApplication.java
@@ -1,4 +1,5 @@
 package es.ull.project;
+// Spring Boot main application class
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
