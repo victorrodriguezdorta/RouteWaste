5c80966c58af4b8f929720cfb22ad8a1083ef58d
alu0101540153 <alu0101540153@ull.edu.es>
2025-12-01 09:59:44 +0000

    just testing the actions

--- back-end/src/main/java/es/ull/project/BackEndApplication.java
+++ back-end/src/main/java/es/ull/project/BackEndApplication.java
@@ -1,5 +1,5 @@
 package es.ull.project;
-// Spring Boot main  application class   
+// Spring Boot main  application class    a
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
